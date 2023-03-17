f3c8fdadda7bf067a205fcb88def4da38625fed4
KennethGalvez <gal20079@uvg.edu.gt>
2023-03-17 00:05:28 -0600

    ,

--- AFD.py
+++ AFD.py
@@ -1,48 +1,111 @@
-from pythomata.automata import Automaton 
-from pythomata.algorithms.to_automaton import to_automaton
+class State:
+    def __init__(self, label=None):
+        self.transitions = {}
+        self.label = label
+        self.final = False
 
+class NFA:
+    def __init__(self, start=None, end=None):
+        self.start = start
+        self.end = end
 
-# Definimos una función que convierte una expresión postfix en un AFN
 def postfix_to_nfa(postfix):
     stack = []
     for c in postfix:
         if c == '.':
-            # Concatenación
             nfa2 = stack.pop()
             nfa1 = stack.pop()
-            nfa1.concatenate(nfa2)
-            stack.append(nfa1)
+            nfa1.end.transitions[''] = nfa2.start
+            stack.append(NFA(nfa1.start, nfa2.end))
         elif c == '|':
-            # Unión
             nfa2 = stack.pop()
             nfa1 = stack.pop()
-            nfa1.union(nfa2)
-            stack.append(nfa1)
+            start = State()
+            start.transitions[''] = nfa1.start
+            start.transitions[''] = nfa2.start
+            end = State()
+            nfa1.end.transitions[''] = end
+            nfa2.end.transitions[''] = end
+            stack.append(NFA(start, end))
         elif c == '*':
-            # Cierre de Kleene
             nfa = stack.pop()
-            nfa.kleene_star()
-            stack.append(nfa)
+            start = State()
+            end = State()
+            start.transitions[''] = nfa.start
+            start.transitions[''] = end
+            nfa.end.transitions[''] = nfa.start
+            nfa.end.transitions[''] = end
+            stack.append(NFA(start, end))
         else:
-            # Carácter
-            nfa = Automaton()
-            nfa.add_transition(nfa.initial_state, nfa.create_state(), c)
-            nfa.add_final_state(nfa.initial_state)
-            stack.append(nfa)
-    return stack.pop()
+            state = State(c)
+            stack.append(NFA(state, state))
+    nfa = stack.pop()
+    nfa.end.final = True
+    return nfa
 
-# Definimos una función que utiliza el algoritmo de construcción de subconjuntos para convertir un AFN en un AFD
-def nfa_to_dfa(nfa):
-    dfa = to_automaton(nfa).minimize()
-    return dfa
+def get_states(nfa):
+    states = set()
+    queue = [nfa.start]
+    while queue:
+        state = queue.pop()
+        if state in states:
+            continue
+        states.add(state)
+        for symbol, next_state in state.transitions.items():
+            queue.append(next_state)
+    return states
+
+def get_alphabet(nfa):
+    alphabet = set()
+    for state in get_states(nfa):
+        for symbol in state.transitions.keys():
+            if symbol:
+                alphabet.add(symbol)
+    return alphabet
+
+def get_transitions(nfa):
+    transitions = {}
+    for state in get_states(nfa):
+        for symbol, next_state in state.transitions.items():
+            if not symbol:
+                symbol = 'epsilon'
+            if state not in transitions:
+                transitions[state] = {}
+            if symbol not in transitions[state]:
+                transitions[state][symbol] = set()
+            transitions[state][symbol].add(next_state)
+    return transitions
+
+def print_transition_table(nfa):
+    def state_cmp(s1, s2):
+        if s1.label is None and s2.label is None:
+            return 0
+        elif s1.label is None:
+            return -1
+        elif s2.label is None:
+            return 1
+        else:
+            return int(s1.label) - int(s2.label)
+
+    states = sorted(list(get_states(nfa)), key=lambda s: (s.label is None, s.label))
+    alphabet = sorted(list(get_alphabet(nfa)))
+    transitions = get_transitions(nfa)
+
+    print('{:<10}'.format(''), end='')
+    for symbol in alphabet:
+        print('{:<10}'.format(symbol), end='')
+    print('')
+
+    for state in states:
+        print('{:<10}'.format(state.label if state.label is not None else 'epsilon'), end='')
+        for symbol in alphabet:
+            next_states = transitions[state][symbol] if symbol in transitions[state] else set()
+            labels = ', '.join(sorted([s.label for s in next_states if s.label is not None]))
+            print('{:<10}'.format(labels), end='')
+        print('')
+
 
 # Ejemplo de uso
-postfix = "ab|c*."
-nfa = postfix_to_nfa(postfix)
-dfa = nfa_to_dfa(nfa)
-
-# Mostramos los autómatas resultantes
-print("AFN resultante:")
-print(nfa.to_dot())
-print("AFD resultante:")
-print(dfa.to_dot())
+postfix_expression = 'a*b*|c'
+nfa = postfix_to_nfa(postfix_expression)
+print_transition_table(nfa)
--- main.py
+++ main.py
@@ -2,25 +2,15 @@ from postfix import *
 from nfa import *
 
 # Uso del programa
-# expresion_regular = '(a|b)*c'
-# expresion_postfix = infix_a_postfix(expresion_regular)
-# if expresion_postfix is not None:
-#     print(f"La expresión regular {expresion_regular} en notación postfix es: {expresion_postfix}")
-# else:
-#     print(f"La expresión regular {expresion_regular} es inválida.")
+expresion_regular = '(a|b)c*'
+expresion_postfix = infix_a_postfix(expresion_regular)
+if expresion_postfix is not None:
+    print(f"La expresión regular {expresion_regular} en notación postfix es: {expresion_postfix}")
+else:
+    print(f"La expresión regular {expresion_regular} es inválida.")
 
 # nfa = postfix_to_nfa(expresion_postfix)
 # visualize_nfa(nfa)
 
-from AFD import postfix_to_nfa, nfa_to_dfa, draw_dfa
-
-# Convertimos una expresión postfix en un NFA
-nfa = postfix_to_nfa('ab.c*.')
-
-# Convertimos el NFA en un AFD
-dfa = nfa_to_dfa(nfa)
-
-# Dibujamos el AFD como un grafo
-draw_dfa(dfa, filename='ejemplo')
 
 
